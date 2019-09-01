1bdcde561af8990c2f479134451c6c1b4cd32554
Phogy <phogy@phogy.com>
2019-09-01 20:14:28 +0200

    Switch back comma and dash.

--- keyboards/planck/keymaps/phogy/keymap.c
+++ keyboards/planck/keymaps/phogy/keymap.c
@@ -60,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             NO_ACUT, KC_F, KC_G, KC_C, KC_R, KC_L,
             KC_A, KC_O, KC_E, KC_U, KC_I, KC_TAB,
             KC_BSPACE, KC_D, KC_H, KC_T, KC_N, KC_S,
-            KC_DOT, KC_Q, KC_J, KC_K, KC_X, NO_UNDS,
+            KC_DOT, KC_Q, KC_J, KC_K, KC_X, KC_COMMA,
             KC_ENTER, KC_B, KC_M, KC_W, KC_V, KC_Z,
             MO(_FUNCTION), KC_LCTRL, KC_LALT, KC_LSHIFT, LOWER, KC_SPACE,
-            KC_NO, RAISE, KC_RSHIFT, KC_RALT, KC_LGUI, KC_COMMA),
+            KC_NO, RAISE, KC_RSHIFT, KC_RALT, KC_LGUI, NO_MINS),
 
 
   [_RAISE] = LAYOUT_planck_grid(
