bcd6b54d0abf1d5bbd5745d6ee0d880363045cd8
Phogy <phogy@phogy.com>
2019-09-12 07:37:56 +0200

    Make music mode working again.

--- keyboards/planck/keymaps/phogy/config.h
+++ keyboards/planck/keymaps/phogy/config.h
@@ -7,9 +7,11 @@
 #define MIDI_ADVANCED
 
 #define ENCODER_RESOLUTION 4
+#ifdef COMBO_ENABLE
 #define COMBO_COUNT 1
 #define COMBO_TERM 80
 #define TAPPING_TERM 125
+#endif
 
 /*
   Set any config.h overrides for your specific keymap here.
